4bc3a34ea118ab91ad057ae622b110bc142bc478
AndreasDucouneau <andreas.ducourneau@viacesi.fr>
2020-06-18 03:53:11 +0200

    LandingPage v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import {
+  Layout,
+  Header,
+  Navigation,
+  Drawer,
+  Content,
+  Footer,
+  FooterSection,
+  FooterLinkList,
+} from "react-mdl";
 import Main from "./components/main";
 import { Link } from "react-router-dom";
 
@@ -8,25 +17,32 @@ function App() {
   return (
     <div className="demo-big-content">
       <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title="Solidatrity Bond" scroll>
           <Navigation>
-            <Link to="/">Link</Link>
-            <Link to="/">Link</Link>
-            <Link to="/">Link</Link>
-            <Link to="/">Link</Link>to
+            <Link to="/aboutus">A propos de nous</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/connexion">Connexion</Link>
           </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer className="sheader-color" title="Solidatrity Bond">
           <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
+            <a href="/profile">Profile</a>
+            <a href="/map">Maps</a>
+            <a href="/tchat">Messagerie</a>
             <a href="/">Link</a>
           </Navigation>
         </Drawer>
         <Content>
           <div className="page-content" />
           <Main />
+          <Footer className="footer-color" size="mini">
+            <FooterSection type="left" logo="Title">
+              <FooterLinkList>
+                <a href="/">Help</a>
+                <a href="/">Privacy & Terms</a>
+              </FooterLinkList>
+            </FooterSection>
+          </Footer>
         </Content>
       </Layout>
     </div>
