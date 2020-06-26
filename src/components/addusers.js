fdf58b616a08741c62b2ab083581126969c589f3
Kilian <kilian.jugie@viacesi.fr>
2020-06-26 14:48:56 +0200

    Working admin pannel

--- src/components/addusers.js
+++ src/components/addusers.js
@@ -1,23 +1,71 @@
 import React, { Component } from "react";
+import jsSHA from "jssha";
 
 class Addusers extends Component {
+  static infos = {};
+
+  static handleChange(event) {
+    Addusers.infos[event.target.name] = event.target.value;
+  }
+
+  static handleSubmit(event) {
+    var passHash = new jsSHA("SHA-512", "TEXT", { encoding: "UTF8" });
+    passHash.update(Addusers.infos["password"]);
+
+    fetch("http://localhost:3000/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+        body: JSON.stringify({
+        "firstname": Addusers.infos["firstname"],
+        "lastname": Addusers.infos["lastname"],
+        "email": Addusers.infos["email"],
+        "description": "",
+        "authkey": passHash.getHash("HEX"),
+        "rolename": Addusers.infos["role"]
+        })}).then((res) => {
+          alert("Utilisateur correctement ajouté");
+        });
+  }
+
   render() {
     return (
       <div>
         <h1>Pour ajouter un utilisateur</h1>
         <h2>Veuillez renseigner les informations nécessaires :</h2>
         <hr className="featurette-divider"></hr>
-        Entrez le nom : <input type="text" name="nom" /> <br />
-        Entrez le prénom : <input type="text" name="nom" /> <br />
-        Entrez l'email : <input type="text" name="nom" /> <br />
-        Entrez le nom :
+        Entrez le nom : <input
+          type="text"
+          name="lastname" 
+          onChange={Addusers.handleChange} 
+          required={true} 
+          /> <br />
+        Entrez le prénom : <input
+          type="text" 
+          name="firstname" 
+          onChange={Addusers.handleChange}
+          required={true} 
+          /> <br />
+        Entrez l'email : <input
+          type="text" 
+          name="email" 
+          onChange={Addusers.handleChange}
+          required={true} 
+         /> <br />
+        Entrez le mot de passe : <input
+          type="password" 
+          name="password" 
+          onChange={Addusers.handleChange}
+          required={true} 
+         /> <br/>
+        Entrez le role :
         <select
           className="custom-select d-block w-100"
           id="state"
           name="role"
-          /*onChange={Register.handleChange}
+          onChange={Addusers.handleChange}
           required={true}
-          */
         >
           <option value="">Choisir...</option>
           <option>Administrateur</option>
@@ -26,7 +74,7 @@ class Addusers extends Component {
           <option>Fabricant</option>
         </select>
         <br />
-        <input type="submit" name="valider" value="Ajouter" />
+        <input type="submit" name="valider" value="Ajouter" onClick={Addusers.handleSubmit}/>
         <hr className="featurette-divider"></hr>
       </div>
     );
