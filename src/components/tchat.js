dda3448487cfcc862f73a9fa73b7281334cf310f
Kilian <kilian.jugie@viacesi.fr>
2020-06-29 11:07:22 +0200

    Debug fixes tchat

--- src/components/tchat.js
+++ src/components/tchat.js
@@ -148,28 +148,9 @@ class Tchat extends Component {
             <Tab>Envoyer un message</Tab>
           </Tabs>
           <section>
-            <div className="content">
-              Content for the tab: {this.state.activeTab}
-            </div>
             {this.state.activeTab === 0 && (
               <div className="my-3 p-3 bg-white rounded shadow-sm">
                 <div className="media text-muted pt-3">
-                  <svg
-                    className="bd-placeholder-img mr-2 rounded"
-                    width="32"
-                    height="32"
-                    xmlns="http://www.w3.org/2000/svg"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                    role="img"
-                    aria-label="Placeholder: 32x32"
-                  >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#007bff"></rect>
-                    <text x="50%" y="50%" fill="#007bff" dy=".3em">
-                      32x32
-                    </text>
-                  </svg>
                   <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
                     <strong className="d-block text-gray-dark">{Tchat.localUserName}</strong>
                     {Tchat.lastMessage}
